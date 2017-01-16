88c8d666c5a42d38b5858037bff4c018132ee6eb
Piotrekk85 <piotr.jastr@gmail.com>
2017-01-16 12:03:47 +0100

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PociagWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Pociag pociag = new Pociag(); //prywatne pole na obiekt "pociąg"
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+            Ukryj();
+        }
+
+        public void Ukryj() //metoda ukrywająca gridy
+        {
+            PanelLokomotywa.Visibility = Visibility.Hidden;
+            PanelWagon.Visibility = Visibility.Hidden;
+        }
+
+        private void DodajLokomotywe_Click(object sender, RoutedEventArgs e) //odkrycie grida z dodawaniem lokomotyw
+        {
+            PanelLokomotywa.Visibility = Visibility.Visible;
+            PanelWagon.Visibility = Visibility.Hidden;
+        }
+
+        private void DodajL_Click(object sender, RoutedEventArgs e) //dodanie lokomotywy do pociągu
+        {
+            try
+            {
+                int masaLok; //zmienna pomocnicza
+                masaLok = Convert.ToInt32(MasaL.Text); //konwersja tekstu na liczbę typu int
+                if (masaLok <=0) 
+                    throw new ArgumentOutOfRangeException(); //sprawdzenie czy masa będzie dodatnia
+                pociag.DodajLokomotywe(ModelL.Text, masaLok);
+                SkladPociagu.Text = pociag.Informacje();
+            }
+            catch
+            {
+                MessageBox.Show("Sprawdź poprawność wprowadzonych danych!");
+            }
+
+        }
+
+        private void Ukryj_Click(object sender, RoutedEventArgs e) //ukrycie gridów
+        {
+            Ukryj();
+        }
+
+        private void DodajWagon_Click(object sender, RoutedEventArgs e) //odkrycie grida z dodawaniem wagonów
+        {
+            PanelLokomotywa.Visibility = Visibility.Hidden;
+            PanelWagon.Visibility = Visibility.Visible;
+        }
+
+        private void DodajW_Click(object sender, RoutedEventArgs e) //dodanie wagonu do pociągu
+        {
+            try
+            {
+                int masaWag; //zmienna pomocnicza
+                masaWag = Convert.ToInt32(MasaW.Text); //konwersja string ns int
+                if (masaWag <= 0)
+                    throw new ArgumentOutOfRangeException(); //sprawdzenie warunku aby masa była dodatnia
+                if (Typ.SelectedIndex == 0)
+                {
+                    pociag.DodajOsobowy(ModelW.Text, masaWag,RodzajLadunek.Text);
+                }
+                else if (Typ.SelectedIndex == 1)
+                {
+                    pociag.DodajTowarowy(ModelW.Text, masaWag, RodzajLadunek.Text);
+                }
+                else //wyrzucenie wyjątku w sytuacji kiedy w liście rozwijanej nie jest nic wybrane
+                {
+                    throw new ArgumentNullException(); 
+                }
+                SkladPociagu.Text = pociag.Informacje();
+            }
+            catch
+            {
+                MessageBox.Show("Sprawdź poprawność wprowadzonych danych!");
+            }
+        }
+
+        private void SprawdzPociag_Click(object sender, RoutedEventArgs e) //wyświetlenie okienka z informacją czy pociąg może jechać
+        {
+            if (pociag.MozeJechac())
+            {
+                MessageBox.Show("Pociąg może jechać!");
+            }
+            else 
+            {
+                MessageBox.Show("Ups, coś jest nie tak! Pociąg nie może jechać!");
+            }
+        }
+    }
+}
---
+++ Pociag.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PociagWPF
+{
+    /// <summary>
+    /// Klasa dla pociągów.
+    /// </summary>
+    
+    class Pociag : IUzupelnijSklad, IPoprawnyPociag //podpięcie interfejsów
+    {
+        //deklaracja pól w klasie
+        private List<Lokomotywa> lokomotywy = new List<Lokomotywa>();
+        private List<Wagon> wagony = new List<Wagon>();
+
+        public bool MozeJechac() //metoda sprawdzająca czy pociag moze jechac
+        {
+            int masaL = 0; //zmienna pomocnicza na masę wszystkich lokomotyw
+            for (int i=0; i< lokomotywy.Count;i++)
+            {
+                masaL += lokomotywy[i].PobierzMase();
+            }
+
+            int masaW = 0; //zmienna pomocniczna na masę wszystkich wagonów
+            foreach (var element in wagony)
+            {
+                masaW += element.PobierzMase();
+            }
+
+            //instrukcja na sprawdzenie warunków
+            if (masaL >= masaW && lokomotywy.Count > 0) return true;
+            else return false;
+        }
+
+        public void DodajLokomotywe(string model, int masa) //metoda dodająca lokomotywę do składu pociągu
+        {
+            lokomotywy.Add(new Lokomotywa(model, masa));
+        }
+
+        public void DodajOsobowy(string model, int masa, string rodzaj) //metoda dodająca wagon osobowy do składu pociągu
+        {
+            wagony.Add(new Osobowy(model, masa,rodzaj));
+        }
+
+        public void DodajTowarowy(string model, int masa, string ladunek) //metoda dodająca wagon towarowy do składu pociągu
+        {
+            wagony.Add(new Towarowy(model, masa, ladunek));
+        }
+
+        public string Informacje() //metoda zwracająca informacje o składzie pociągu
+        {
+            string info = "Skład pociągu:"; //zmienna pomocnicza
+            for (int i =0; i<lokomotywy.Count;i++)
+            {
+                info += Environment.NewLine + lokomotywy[i].Informacje();
+            }
+            foreach(var element in wagony)
+            {
+                info += Environment.NewLine + element.Informacje();
+            }
+            return info;
+            
+        }
+    }
+}
